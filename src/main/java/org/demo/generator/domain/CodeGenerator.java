cae9603c76989de239b7a6b886fe9d2677417d94
Tomatos <3333331969@qq.com>
2025-09-08 19:19:20 +0800

    feat: 添加自定义CURD方法请求路径

--- src/main/java/org/demo/generator/domain/CodeGenerator.java
+++ src/main/java/org/demo/generator/domain/CodeGenerator.java
@@ -14,6 +14,7 @@ import org.thymeleaf.context.Context;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.demo.generator.enums.GenerateTypeEnum.*;
 
@@ -31,7 +32,9 @@ public class CodeGenerator {
 
     public static GeneratorConfig generatorConfig() {
         return SpringContextUtil.getBean(GeneratorConfig.class);
-    };
+    }
+
+    ;
 
     public static String generateEntity(GenerateParam param) {
         log.info("生成{}表的Entity代码", param.getTableName());
@@ -82,26 +85,32 @@ public class CodeGenerator {
         return templateEngine().process("MapperImpl", getMapperXMLContext(param));
     }
 
-    private static List<ColumnInfo> convert(List<ColumnInfo> columnInfos) {
-        return columnInfos.stream()
-                          .peek(columnInfo -> {
-                              columnInfo.setType(DbTypeConvert.convertToJavaType(columnInfo.getType()));
-                              columnInfo.setName(StrUtil.toCamelCase(columnInfo.getDbName()));
-                              // 当前列是否为String类型
-                              columnInfo.setString("String".equals(columnInfo.getType()));
-                          })
-                          .filter(columnInfo -> !generatorConfig()
-                                  .getClassIgnoreFields()
-                                  .contains(columnInfo.getName())
-                          )
-                          .toList();
+    private static List<ColumnInfo> convert(List<ColumnInfo> columnInfos, boolean isFilterCommonField) {
+        Stream<ColumnInfo> stream = columnInfos.stream()
+                                               .peek(columnInfo -> {
+                                                   columnInfo.setType(
+                                                           DbTypeConvert.convertToJavaType(
+                                                                   columnInfo.getType()));
+                                                   columnInfo.setName(StrUtil.toCamelCase(
+                                                           columnInfo.getDbName()));
+                                                   // 当前列是否为String类型
+                                                   columnInfo.setString(
+                                                           "String".equals(columnInfo.getType()));
+                                               });
+        if (isFilterCommonField) {
+            stream = stream.filter(columnInfo -> !generatorConfig()
+                    .getClassIgnoreFields()
+                    .contains(columnInfo.getName())
+            );
+        }
+        return stream.toList();
     }
 
     private static Context getEntityContext(GenerateParam param) {
         Context context = new Context();
         setCommonVar(context, param);
         context.setVariable("tableComment", param.getTableComment());
-        context.setVariable("columns", convert(param.getColumns()));
+        context.setVariable("columns", convert(param.getColumns(), true));
 
         String entityClassName = buildConcatenatedClassName(ENTITY, param.getClassName());
         context.setVariable("entityClassName", entityClassName);
@@ -113,7 +122,7 @@ public class CodeGenerator {
         Context context = new Context();
         setCommonVar(context, param);
         context.setVariable("tableComment", param.getTableComment());
-        context.setVariable("columns", convert(param.getColumns()));
+        context.setVariable("columns", convert(param.getColumns(), true));
 
         String dtoEntityClassName = buildConcatenatedClassName(DTO, param.getClassName());
         context.setVariable("dtoEntityClassName", dtoEntityClassName);
@@ -124,7 +133,7 @@ public class CodeGenerator {
     private static Context getConditionContext(GenerateParam param) {
         Context context = new Context();
         setCommonVar(context, param);
-        context.setVariable("columns", convert(param.getColumns()));
+        context.setVariable("columns", convert(param.getColumns(), true));
 
         context.setVariable("conditionClassName", buildConcatenatedClassName(CONDITION,
                                                                              param.getClassName()));
@@ -143,14 +152,17 @@ public class CodeGenerator {
         context.setVariable("mapperModelName", getModelName(MAPPER));
         context.setVariable("mapperInstance", mapperInstance);
         context.setVariable("serviceModelName", getModelName(SERVICE));
-        context.setVariable("serviceClassName", buildConcatenatedClassName(SERVICE, param.getClassName()));
-        context.setVariable("serviceInterfaceClassName", buildConcatenatedClassName(SERVICE_INTERFACE,
-                                                                            param.getClassName()));
+        context.setVariable("serviceClassName",
+                            buildConcatenatedClassName(SERVICE, param.getClassName()));
+        context.setVariable("serviceInterfaceClassName",
+                            buildConcatenatedClassName(SERVICE_INTERFACE,
+                                                       param.getClassName()));
         context.setVariable("serviceInterfaceModelName", getModelName(SERVICE_INTERFACE));
         context.setVariable("dtoClassName", buildConcatenatedClassName(DTO,
                                                                        param.getClassName()));
         context.setVariable("dtoModelName", getModelName(DTO));
-        context.setVariable("entityClassName", buildConcatenatedClassName(ENTITY, param.getClassName()));
+        context.setVariable("entityClassName",
+                            buildConcatenatedClassName(ENTITY, param.getClassName()));
         context.setVariable("entityModelName", getModelName(ENTITY));
         context.setVariable("conditionClassName", buildConcatenatedClassName(CONDITION,
                                                                              param.getClassName()));
@@ -161,7 +173,8 @@ public class CodeGenerator {
     private static Context getIServiceContext(GenerateParam param) {
         Context context = new Context();
         setCommonVar(context, param);
-        String serviceInterfaceClassName = buildConcatenatedClassName(SERVICE_INTERFACE, param.getClassName());
+        String serviceInterfaceClassName = buildConcatenatedClassName(SERVICE_INTERFACE,
+                                                                      param.getClassName());
         context.setVariable("serviceInterfaceClassName", serviceInterfaceClassName);
         context.setVariable("serviceInterfaceModelName", getModelName(SERVICE_INTERFACE));
         context.setVariable("dtoClassName", buildConcatenatedClassName(DTO,
@@ -178,19 +191,28 @@ public class CodeGenerator {
         setCommonVar(context, param);
         context.setVariable("requestPath", param.getRequestPath());
 
-        String serviceInstance = StrUtil.lowerFirst(buildConcatenatedClassName(SERVICE, param.getClassName()));
+        String serviceInstance = StrUtil.lowerFirst(
+                buildConcatenatedClassName(SERVICE, param.getClassName()));
 
-        context.setVariable("serviceInterfaceClassName", buildConcatenatedClassName(SERVICE_INTERFACE, param.getClassName()));
+        context.setVariable("serviceInterfaceClassName",
+                            buildConcatenatedClassName(SERVICE_INTERFACE, param.getClassName()));
         context.setVariable("serviceInterfaceModelName", getModelName(SERVICE_INTERFACE));
         context.setVariable("serviceInstance", serviceInstance);
-        context.setVariable("controllerClassName", buildConcatenatedClassName(CONTROLLER, param.getClassName()));
+        context.setVariable("controllerClassName",
+                            buildConcatenatedClassName(CONTROLLER, param.getClassName()));
         context.setVariable("controllerModelName", getModelName(CONTROLLER));
         context.setVariable("dtoClassName", buildConcatenatedClassName(DTO,
                                                                        param.getClassName()));
         context.setVariable("dtoModelName", getModelName(DTO));
         context.setVariable("conditionClassName", buildConcatenatedClassName(CONDITION,
-                                                                       param.getClassName()));
+                                                                             param.getClassName()));
         context.setVariable("conditionModelName", getModelName(CONDITION));
+        context.setVariable("commonRequestPath", generatorConfig().getCommonRequestPath());
+
+        generatorConfig().getCustomMethodRequest()
+                         .forEach((key, value) ->
+                                          context.setVariable(key + "RequestPath", value)
+                         );
         return context;
     }
 
@@ -212,13 +234,17 @@ public class CodeGenerator {
 
     private static void setCommonVar(Context context, GenerateParam param) {
         context.setVariable("author", param.getAuthor());
-        context.setVariable("nowDateTime", LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+        context.setVariable("nowDateTime", LocalDate.now()
+                                                    .format(DateTimeFormatter.ofPattern(
+                                                            "yyyy-MM-dd")));
         context.setVariable("packageName", param.getPackageName());
         context.setVariable("className", param.getClassName());
-        generatorConfig().getCustomMethodName().forEach(context::setVariable);
+        generatorConfig().getCustomMethodName()
+                         .forEach(context::setVariable);
 
         List<ColumnInfo> columns = param.getColumns();
-        columns.get(columns.size() - 1).setEndColumn(true);
+        columns.get(columns.size() - 1)
+               .setEndColumn(true);
     }
 
     private static Context getMapperXMLContext(GenerateParam param) {
@@ -235,8 +261,10 @@ public class CodeGenerator {
         context.setVariable("mapperFullClassName", mapperFullClassName);
         context.setVariable("entityFullClassName", entityFullClassName);
         context.setVariable("conditionEntityFullClassName", conditionFullClassName);
-        context.setVariable("columns", convert(param.getColumns()));
+        context.setVariable("columns", convert(param.getColumns(), false));
         context.setVariable("className", param.getClassName());
+        generatorConfig().getCustomMethodName()
+                         .forEach(context::setVariable);
         return context;
     }
 
